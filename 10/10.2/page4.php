f6c1c08df49a4fb4f3d777ad1047fcd7107b8d24
kama04 <86296727+kama04@users.noreply.github.com>
2022-04-15 16:37:00 +0200

    Update page4.php

--- 10/10.2/page4.php
+++ 10/10.2/page4.php
@@ -1,1 +1,11 @@
 <?php
+
+session_start();
+
+$_SESSION['pages'][] = $_SERVER['PHP_SELF'];
+if (isset($_POST["surname"]) && isset($_POST["name"])&& isset($_POST["age"])) {
+    echo ($_SESSION['name']);
+    echo($_SESSION['surname']);
+    echo "Ваш возраст$_SESSION[age]лет";
+}
+?>

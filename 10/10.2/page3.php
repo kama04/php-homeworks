21961767c3b6a8f3f371b9b59ebebc4c6c273e3d
kama04 <86296727+kama04@users.noreply.github.com>
2022-04-15 16:36:31 +0200

    Update page3.php

--- 10/10.2/page3.php
+++ 10/10.2/page3.php
@@ -1,1 +1,15 @@
 <?php
+
+session_start();
+
+$_SESSION['pages'][] = $_SERVER['PHP_SELF'];
+if (isset($_POST["surname"])) {
+    $_SESSION["surname"] = strip_tags(trim($_POST["surname"]));
+}
+?>
+
+<form action="page4.php" method="post">
+    Возраст:<br><br>
+    <input type="text" name="age"><br><br>
+    <input type="submit" value="дальше">
+</form>
